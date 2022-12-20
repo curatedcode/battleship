a556e16cdd293466e36421cc6496dc83bb49f244
zack <zackaryfotheringham@hotmail.com>
2022-12-20 12:15:26 -0500

    increase hitCount on hit and check for sunk status

--- src/ships.js
+++ src/ships.js
@@ -1,47 +1,43 @@
 
 const Ship = ()=>{
-
-    let carrier = {
-        length: 5,
-        hitCount: 0,
-        sunk: false
-    }
-    let battleship = {
-        length: 5,
-        hitCount: 0,
-        sunk: false
-    }
-    let cruiser = {
-        length: 3,
-        hitCount: 0,
-        sunk: false
-    }
-    let submarine = {
-        length: 3,
-        hitCount: 0,
-        sunk: false
-    }
-    let destroyer = {
-        length: 2,
-        hitCount: 0,
-        sunk: false
-    }
-
-    function hit(ship){
-        return ship.hitCount = hitCount + 1
-    }
-
-    function isSunk(ship){
-        ship.sunk = true
-    }
     return {
-        carrier: carrier,
-        battleship: battleship,
-        cruiser: cruiser,
-        submarine: submarine,
-        destroyer: destroyer,
-        hit: hit,
-        isSunk: isSunk
+        carrier: {
+            length: 5,
+            hitCount: 0,
+            sunk: false
+        },
+        battleship: {
+            length: 5,
+            hitCount: 0,
+            sunk: false
+        },
+        cruiser: {
+            length: 3,
+            hitCount: 0,
+            sunk: false
+        },
+        submarine: {
+            length: 3,
+            hitCount: 0,
+            sunk: false
+        },
+        destroyer: {
+            length: 2,
+            hitCount: 0,
+            sunk: false
+        },
+        hit (ship){
+            if(ship === 'carrier') {ship = this.carrier; console.log(ship)}
+            else if (ship === 'battleship') {ship = this.battleship}
+            else if (ship === 'cruiser') {ship = this.cruiser}
+            else if (ship === 'submarine') {ship = this.submarine}
+            else if (ship === 'destroyer') {ship = this.destroyer}
+            ship.hitCount++
+            if(ship.hitCount === ship.length) console.log(ship); return this.isSunk(ship)
+        },
+        isSunk (ship){
+            return ship.sunk = true
+        }
     }
 }
 
