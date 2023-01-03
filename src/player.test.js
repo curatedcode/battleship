3c72e0d84fcc7f41ca49e4946ae7508d59d34696
zack <zackaryfotheringham@hotmail.com>
2023-01-03 17:42:36 -0500

    coords no longer modified on hit

--- src/player.test.js
+++ src/player.test.js
@@ -1,17 +1,10 @@
 const playerModule = require('./player')
 
-describe('sendAttack to opponents board',()=>{
+test('send attack to opponents board',()=>{
     const playerOne = playerModule.Player()
     const playerTwo = playerModule.Player()
-    beforeAll(()=>{
-        playerOne.sendAttack(16,playerTwo.gameBoard,playerTwo.ships)
-    })
-    test('sendAttack to opponents board',()=>{
-        expect(playerTwo.gameBoard.shipCoordinates[4].coords).toStrictEqual([17])
-    })
-    test('verify playerOne gameBoard did not get modified',()=>{
-        expect(playerOne.gameBoard.shipCoordinates[4].coords).toStrictEqual([16,17])
-    })
+    playerOne.sendAttack(16,playerTwo.gameBoard,playerTwo.ships)
+    expect(playerTwo.ships.allShips[4].info.hitCount).toBe(1)
 })
 
 test('generate a random move thats has not been played before',()=>{
