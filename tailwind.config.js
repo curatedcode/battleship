2d4e629884641c05c37bf4d5f29f3b5ac6277564
zack <zackaryfotheringham@hotmail.com>
2023-01-08 11:30:03 -0500

    import and set new font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
     content: ["./index.html"],
     theme: {
         extend: {},
+        fontFamily: {
+            sans: ['Seymour\\ One','sans','sans-serif']
+        }
     },
     plugins: [],
 }
