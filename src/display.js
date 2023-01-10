e79a15e949d39372e463479f302a4d1b6c010659
zack <zackaryfotheringham@hotmail.com>
2023-01-10 06:39:50 -0500

    delay winner until respective shot has been shown

--- src/display.js
+++ src/display.js
@@ -383,8 +383,12 @@ function showPlacedShots(gameBoard,player){
 
 function checkForWinner(){
     if(player.gameBoard.isGameOver(player.ships)){
-        document.querySelector('.lose-box').classList.remove('hidden')
+        setTimeout(() => {
+            document.querySelector('.lose-box').classList.remove('hidden')
+        }, 3500);
     } else if (computer.gameBoard.isGameOver(computer.ships)){
-        document.querySelector('.win-box').classList.remove('hidden')
+        setTimeout(() => {
+            document.querySelector('.win-box').classList.remove('hidden')
+        }, 2500);
     }
 }
